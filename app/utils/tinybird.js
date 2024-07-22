2400cc838d75e75b4921268871e3b66b6b05747e
tb-peregrine <cameronarcher@tinybird.co>
2024-07-22 14:44:16 -0500

    simplify tinybird

--- app/utils/tinybird.js
+++ app/utils/tinybird.js
@@ -1,27 +1,3 @@
-const fastestGameUrl = (host, token) => {
-    return `https://${host}/v0/pipes/fastest_game.json?user=&token=${token}`
-}
-
-const fastestClickUrl = (host, token) => {
-    return `https://${host}/v0/pipes/fastest_click.json?user=&token=${token}`
-}
-
-const favoriteTargetUrl = (host, token) => {
-    return `https://${host}/v0/pipes/favorite_target.json?user=&token=${token}`
-}
-
-const nemesisTargetUrl = (host, token) => {
-    return `https://${host}/v0/pipes/nemesis_target.json?user=&token=${token}`
-}
-
-const gameTrackerUrl = (host, token) => {
-    return `https://${host}/v0/pipes/game_tracker.json?user=&token=${token}`
-}
-
-const leaderboardUrl = (host, token) => {
-    return `https://${host}/v0/pipes/leaderboard.json?&token=${token}`
-}
-
 const sendEvent = async (payload, token, host, datasource) => {
     try {
         fetch(`https://${host}/v0/events?name=${datasource}`,
@@ -40,24 +16,6 @@ const sendEvent = async (payload, token, host, datasource) => {
   }
 }
 
-const fetchTinybirdApi = async (url, setData) => {
-    try {
-        const response = await fetch(url);
-        const jsonData = await response.json();
-        setData(jsonData.data);
-    } catch (error) {
-        console.error('Error fetching data: ', error)
-    }
-    
-}
-
 export {
-    sendEvent,
-    fetchTinybirdApi,
-    fastestGameUrl,
-    fastestClickUrl,
-    favoriteTargetUrl,
-    nemesisTargetUrl,
-    gameTrackerUrl,
-    leaderboardUrl
+    sendEvent
 }
